c605466fb81bf072e07527e75abfbbe3c351bce0
Danny Libin <omegasol11@gmail.com>
2016-03-12 15:31:53 -0600

    finished app!

--- server.js
+++ server.js
@@ -2,10 +2,15 @@
 var express = require('express');
 var path = require('path');
 var morgan = require('morgan');
+var multer = require('multer');
 var app = express();
+var upload = multer();
 app.use(morgan('dev'));
 var pathname = path.join(process.cwd());
 app.use(express.static(pathname));
+app.post('/upload', upload.single('uploadedFile'), function (req, res) {
+    res.status(200).end(req.file.size.toString());
+});
 var port = process.env.PORT || 3000;
 app.listen(port, function () { return console.log('Listening on port ' + port + '...'); });
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -3,11 +3,17 @@ import * as express from 'express';
 import * as path from 'path';
 import * as morgan from 'morgan';
 import * as axios from 'axios';
+import * as multer from 'multer';
 const app: express.Express = express();
+const upload = multer();
 
 app.use(morgan('dev'));
 let pathname = path.join(process.cwd());
 app.use( express.static(pathname) );
+
+app.post('/upload', upload.single('uploadedFile'), (req, res) => {
+	res.status(200).end(req.file.size.toString());
+});
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Listening on port ' + port + '...'));
